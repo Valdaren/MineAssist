5d6efa13a3faa6876113d60b897f49d0bf6d8859
Valdaren <93627640+Valdaren@users.noreply.github.com>
2024-11-19 20:05:00 -0600

    Add files via upload

---
+++ home_script.js
@@ -0,0 +1,11 @@
+document.getElementById('mobsBtn').addEventListener('click', function() {
+    window.location.href = 'mobs.html';
+});
+
+document.getElementById('blocksBtn').addEventListener('click', function() {
+    window.location.href = 'blocks.html';
+});
+
+document.getElementById('itemsBtn').addEventListener('click', function() {
+    window.location.href = 'items.html';
+});
